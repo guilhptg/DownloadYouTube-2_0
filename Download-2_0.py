1d68e3690b1c41017c6121516c2a606bf3c80fd1
Guilherme Portugal <flordavidaho@gmail.com>
2025-04-15 15:41:24 -0300

    update
    Can chose video or audio download

--- Download-2_0.py
+++ Download-2_0.py
@@ -1,17 +1,40 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 import customtkinter as ctk
-from customtkinter import filedialog
 from pytubefix import *
 from pytubefix.cli import on_progress
 
 
 
-# def on_progress(stream, chunk, bytes_remaining):
-#     total_size = stream.filesize
-#     bytes_downloaded = total_size - bytes_remaining
-#     percent = (bytes_downloaded / total_size) * 100
-#     progress_label.config(text=f"Progresso: {percent:.2f}%")
+def show_on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percent = (bytes_downloaded / total_size) * 100
+    progress_label = f"Progresso: {on_progress:.2f}%"
+
+
+def down_test():
+    url = url_entry.get()
+
+    save_path = ctk.filedialog.askdirectory(title='Escolha a pasta para salvar o arquivo')
+    if not save_path:
+        messagebox.showerror("Erro", "Por favor, escolha uma pasta para salvar o arquivo.")
+    yt = YouTube(url=url, on_progress_callback=on_progress)
+
+    custom_name = filename_entry.get().strip()
+    if not custom_name or checkbox.get() == 'on':
+        print('Titulo Original do Vídeo')
+        custom_name = yt.title
+
+    # Download Audio
+    if radio_var.get() == 1:
+        ys = yt.streams.get_audio_only()
+
+    #Download Video
+    else:
+        ys = yt.streams.get_highest_resolution()
+
+    ys.download(output_path=save_path, filename=custom_name)
 
 
 def download_video():
@@ -82,16 +105,45 @@ label_link = ctk.CTkLabel(app, text='Link do YouTube: ', font=('Arial', 20))
 label_link.pack(pady=10)
 
 
-def down_test():
-    url = url_entry.get()
-    yt = YouTube(url=url, on_progress_callback=on_progress)
-    video_title = yt.title
-    ys = yt.streams.get_audio_only()
-    ys.download()
-
 # Campo para inserção do link
 url_entry = ctk.CTkEntry(app, width=350)
 url_entry.pack(padx=10, pady=10)
+
+
+# Campo para inserção do nome do arquivo final
+label_filename = ctk.CTkLabel(app, text='Nome do arquivo (sem extensão):', font=('Arial', 20))
+label_filename.pack(pady=10)
+
+filename_entry = ctk.CTkEntry(app, width=350)
+filename_entry.pack(pady=10)
+
+# Campo para decidir o nome do arquivo final
+def checkbox_event():
+    # Condicional para nome original do vídeo
+
+    print("checkbox toggled, current value:", check_var.get())
+
+# Checkbox para seleceção de nome original ou não
+check_var = ctk.StringVar(value="on")
+checkbox = ctk.CTkCheckBox(app, text="Nome original do Vídeo", command=checkbox_event,
+                                    variable=check_var, onvalue="on", offvalue="off")
+checkbox.pack(padx=10, pady=10)
+
+def radiobutton_event():
+    print("radiobutton toggled, current value:", radio_var.get())
+
+
+# Campo para escolher o formato que vai ser baixado
+radio_label = ctk.CTkLabel(app, text='Escolha qual formato deseja baixar')
+radio_var = tk.IntVar(value=2)
+radiobutton_1 = ctk.CTkRadioButton(app, text="Audio",
+                                            command=radiobutton_event, variable= radio_var, value=1)
+radiobutton_2 = ctk.CTkRadioButton(app, text="Vídeo",
+                                            command=radiobutton_event, variable= radio_var, value=2)
+
+
+radiobutton_1.pack(padx=10, pady=10)
+radiobutton_2.pack(padx=10, pady=10)
 
 
 botao_baixar = ctk.CTkButton(app, text='Baixar', command=down_test)
